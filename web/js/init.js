6128ed2e071641d2946043c72c0f5332f4e940c7
mer1 <chaoui.mar2@gmail.com>
2017-04-28 17:43:03 +0100

    template integrated

---
+++ web/js/init.js
@@ -0,0 +1,8 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.tooltipped').tooltip({delay: 5});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
