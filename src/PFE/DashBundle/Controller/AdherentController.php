ea080f5db3df58a67518eeb5e54fdfced2027102
Marwane <chaoui.mar2@gmail.com>
2017-05-31 15:32:44 +0000

    CRUD adherent done

--- src/PFE/DashBundle/Controller/AdherentController.php
+++ src/PFE/DashBundle/Controller/AdherentController.php
@@ -67,7 +67,7 @@ class AdherentController extends Controller
             'method' => 'POST',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Create'));
+        $form->add('submit', 'submit', array('label' => '<i class="mdi-av-playlist-add left"></i> Enregistrer'));
 
         return $form;
     }
@@ -147,7 +147,7 @@ class AdherentController extends Controller
             'method' => 'PUT',
         ));
 
-        $form->add('submit', 'submit', array('label' => 'Update'));
+        $form->add('submit', 'submit', array('label' => '<i class="mdi-action-cached left"></i> Mettre à jour'));
 
         return $form;
     }
@@ -217,7 +217,8 @@ class AdherentController extends Controller
         return $this->createFormBuilder()
             ->setAction($this->generateUrl('adherent_delete', array('id' => $id)))
             ->setMethod('DELETE')
-            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->add('submit', 'submit', array('label' => '<i class="mdi-action-delete left"></i> Supprimer',
+                                                        'attr'=>array('class'=>'red')))
             ->getForm()
         ;
     }
