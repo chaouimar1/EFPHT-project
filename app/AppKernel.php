0b72576651d738b3653a0cf8d4d0f263cd93688f
mer1 <chaoui.mar2@gmail.com>
2017-07-09 21:27:52 +0100

    FOSUserBundle Installed

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new PFE\DashBundle\PFEDashBundle(),
             new Ob\HighchartsBundle\ObHighchartsBundle(),
+            new PFE\UserBundle\PFEUserBundle(),
+            new \FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/PFE/UserBundle/Entity/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace PFE\UserBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Entity(repositoryClass="PFE\UserBundle\Entity\UserRepository")
+ * @ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="guid")
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $prenom;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return User
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return User
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+}
